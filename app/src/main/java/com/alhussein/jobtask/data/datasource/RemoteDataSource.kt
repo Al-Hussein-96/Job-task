eb90d7d9bba68cd3e0d9a7e6bd2accd602b854b2
Al-Hussein <tvclockdev@gmail.com>
2022-01-20 10:38:16 +0200

    - init project

---
+++ app/src/main/java/com/alhussein/jobtask/data/datasource/RemoteDataSource.kt
@@ -0,0 +1,4 @@
+package com.alhussein.jobtask.data.datasource
+
+class RemoteDataSource {
+}
--- app/src/main/java/com/alhussein/jobtask/ui/MainActivity.kt
+++ app/src/main/java/com/alhussein/jobtask/ui/MainActivity.kt
@@ -1,11 +1,15 @@
-package com.alhussein.jobtask
+package com.alhussein.jobtask.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.alhussein.jobtask.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
     }
 }
